826f9483d3f2bf776b859b454216d6749c5c1cb8
Irin <averyirin@gmail.com>
2017-02-14 17:41:40 -0500

    Created prompt for marking task as complete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController,AlertController } from 'ionic-angular';
-import { ToDoCreatePage } from '../to-do-create/to-do-create'; 
+import { NavController, AlertController } from 'ionic-angular';
+import { ToDoCreatePage } from '../to-do-create/to-do-create';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Component({
     selector: 'page-home',
     templateUrl: 'home.html'
 })
 export class HomePage {
-    toDoList: FirebaseListObservable<any>; 
+    toDoList: FirebaseListObservable<any>;
     constructor(public navCtrl: NavController, public af: AngularFire, public alertCtrl: AlertController) {
-         this.toDoList = af.database.list('/todos'); } newToDo() { this.navCtrl.push(ToDoCreatePage); 
+        this.toDoList = af.database.list('/todos');
+    } newToDo() {
+        this.navCtrl.push(ToDoCreatePage);
     }
+    promptComplete(toDoId: string) { 
+        let alert = this.alertCtrl.create({ 
+            message: "Mark as Completed?", buttons: [{ text: 'Cancel', }, 
+        { text: 'Mark as Completed', handler: data => { 
+            this.toDoList.update(toDoId, { done: true }); } }] }
+        ); 
+        
+        alert.present(); 
+    }
+
 
 }
