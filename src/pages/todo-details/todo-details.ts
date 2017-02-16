458da625479c5e79781460314f00f44a76727f02
Irin <averyirin@gmail.com>
2017-02-16 10:41:20 -0500

    Added input validation

--- src/pages/todo-details/todo-details.ts
+++ src/pages/todo-details/todo-details.ts
@@ -4,13 +4,13 @@ Author: Irin Avery 200260949
 Date Last Modified: 2/16/2017
 File: todo-details.ts
 Description: A form that edits or creates new tasks
-Update: Now navigates back to the task list after update
+Update: Added input validation
 */
 //import components
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'page-todo-details',
@@ -24,21 +24,36 @@ export class ToDoCreatePage {
   id:string;
   name:string;
   notes:string;
+  private detailsForm : FormGroup;
+  submitAttempt:boolean;
 
   //gathers todolist from the database, and the passed values of task information for editing
-  constructor(public navCtrl: NavController, public af: AngularFire, public navParams: NavParams) {
+  constructor(public formBuilder: FormBuilder, public navCtrl: NavController, public af: AngularFire, public navParams: NavParams) {
+    //input validation
+    this.detailsForm = formBuilder.group({
+    todoName: ['', Validators.compose([Validators.minLength(1), Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+    todoNotes: ['', Validators.compose([Validators.minLength(1),Validators.pattern('[a-zA-Z ]*'), Validators.required])]
+    });
+   
+
     this.toDoList = af.database.list('/toDos');
     this.id = this.navParams.get('id');
-    this.name = this.navParams.get('name');
+    this.name = this.navParams.get('name'); 
     this.notes = this.navParams.get('notes');
   }
   //update to saveToDo
-  saveToDo(id="", name, notes){
-    if(id==""){
-      this.createToDo(name, notes);
+  saveToDo(){
+    this.submitAttempt = true;
+    if(this.detailsForm.valid){
+        
+    if(this.id==""){
+      this.createToDo(this.name, this.notes);
     }else{
-      this.updateToDo(id, name, notes);
+      this.updateToDo(this.id, this.name, this.notes);
     }
+  }else{
+    console.log("Invalid");
+  }
   }
   //Updates the task and then navigates back to the task list view
   updateToDo(id, name, notes){
